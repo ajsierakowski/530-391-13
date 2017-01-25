4690ff37b72f7b302dff8f3d110b9eef9e254800
Adam Sierakowski <sierakowski@jhu.edu>
2017-01-25 14:16:56 -0500

    omp search

---
+++ parallel-tutorial/omp-search.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+
+int main(int argn, char* argv[]) {
+  // error checking
+  if(argn < 3) {
+    printf("Usage: ./serial-search N np\n");
+    return EXIT_FAILURE;
+  }
+
+  // set up random number generator
+  time_t t;
+  srand(1);//(unsigned) time(&t));
+
+  // set up array
+  int n = atoi(argv[1]);
+  int np = atoi(argv[2]);
+  int A[n];
+  int i = 0;
+  for(i = 0; i < n; i++) {
+    A[i] = rand();
+    //printf("A[%d] = %d\n", i, A[i]);
+  }
+
+  // repeat 1,000 times
+  int j = 0;
+  clock_t start = clock();
+  clock_t diff;
+  for(j = 0; j < 1000; j++) {
+    // openMP search
+    int sub_max[np];
+    for(i = 0; i < np; i++) {
+      sub_max[i] = 0;
+    }
+    int max = 0;
+
+#pragma omp parallel num_threads(np)
+    {
+      // setting up the search
+      int tid = omp_get_thread_num();
+      i = 0;
+      int split = n / np;
+
+      // do search over this thread's piece
+      for(i = tid*split; i < (tid+1)*split; i++) {
+        //printf("tid = %d i = %d\n", tid, i);
+        if(A[i] > sub_max[tid]) {
+          sub_max[tid] = A[i];
+        }
+      }
+    }
+    for(i = 0; i < np; i++) {
+      if(sub_max[i] > max) {
+        max = sub_max[i];
+      }
+    }    
+
+    if(j == 999) {
+      printf("The maximum is %d\n", max);
+    }
+  }
+  diff = clock() - start;
+
+  int msec = diff * 1000 / CLOCKS_PER_SEC;
+  printf("Time taken %d.%d seconds\n", msec/1000, msec%1000);
+
+  return EXIT_SUCCESS;
+}
